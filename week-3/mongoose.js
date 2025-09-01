60c2e05cadc34cf6a8438c80651de9665f810478
bensenpai <bishwanath2712@gmail.com>
2025-09-01 10:56:31 +0530

    Edits were made

--- week-3/mongoose.js
+++ week-3/mongoose.js
@@ -1,23 +1,30 @@
+//using a backend server to send matter to the mongodb database!
+
 //we are seeign the working of databasese
 // Particularly we are using MongoDB
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
 mongoose.connect(
-  "mongodb+srv://admin1:84BPPmdGGcc2xFgb@cluster0.xapwwyz.mongodb.net/UserData"
-);
-app.use(express());
+  "mongodb+srv://admin1:84BPPmdGGcc2xFgb@cluster0.xapwwyz.mongodb.net/UserData");
+
+//for our body we use
+app.use(express.json());
+
 //creating my schema
-const User = mongoose.model("User", {
+const User = mongoose.model("users", {
   name: String,
   email: String,
   password: String,
 });
 
 //now putting our user into the user schema we just defined
-
 //without this .save() nothing will work
-app.get("/signin", function (req, res) {
+app.post("/signin",async function (req, res) {
+  
+//the lines of codes below will take deconstruct the body and take out the matter from the body of the get request
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
@@ -27,11 +34,18 @@ app.get("/signin", function (req, res) {
     email: email,
     password: password,
   });
-
   //write code to check if the data alreadyy exists in the database
-  const checking = await findOne({name: }){}
-  user.save(console.log("the matter has been added to the database !"));
-});
+const checking = await User.findOne({ email: email });
+if (checking){
+  return res.status(400).send("user already exists")
+}
+await User.create({name ,email : username,password})
+res.json({
+  "msg" : "user has been successfully created !"
+})
+
+//Since this is not an inmemory database, then we have to use the function inside the mongoose lib that can help us search matter
 
 
-app.listen("3000")
+
+app.listen("3000");
